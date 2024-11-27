ff2d9c5c2c6a70cb2822914a241ac54842afe5ad
iamfiro <appturbo102@gmail.com>
2024-11-27 21:45:19 +0900

    Scene을 효율적으로 이동하기 위한 sceneManager.tsx 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import IntroScene from "./game/scene/introScene.tsx";
+import IntroScene from "./game/scene/intro/introScene";
+import {useEffect, useState} from "react";
+import {createSceneManager, SceneRenderer} from "./engine/scene/sceneManager.tsx";
 
 function App() {
+    const [isInitialized, setIsInitialized] = useState(false);
 
-  return (
-    <>
-      <IntroScene />
-    </>
-  )
+    useEffect(() => {
+        createSceneManager([
+            {
+                path: '/intro',
+                component: () => <IntroScene />
+            }
+        ]);
+        setIsInitialized(true);
+    }, []);
+
+    if (!isInitialized) {
+        return null; // 또는 로딩 인디케이터를 표시할 수 있습니다
+    }
+
+    return (
+        <>
+            <SceneRenderer initialScene={'/intro'} />
+        </>
+    )
 }
 
 export default App
