e994c0b80df0107ee9bef3f6afe052a56935b86a
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-28 14:22:14 +0900

    bootProgram 페이지 추가

---
+++ src/game/scene/intranet/bootProgram.tsx
@@ -0,0 +1,11 @@
+import styles from '../../../styles/scene/intranet/bootProgram.module.scss';
+
+const BootProgram = () => {
+	return (
+		<main className={styles.container}>
+
+		</main>
+	)
+}
+
+export default BootProgram;
