a333156a7fd35db89aaa2a977bd47c3d86a0fd10
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-29 08:30:59 +0900

    missionList.tsx 컴포넌트 분리

--- src/game/scene/intranet/missionList.tsx
+++ src/game/scene/intranet/missionList.tsx
@@ -1,90 +1,7 @@
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
 import style from '../../../styles/scene/intranet/missionList.module.scss';
-
-interface HorizontalLoopConfig {
-    repeat: number;
-    speed: number;
-    snap: boolean;
-    paddingRight: number;
-}
-
-interface TextRefs extends HTMLSpanElement {
-    offsetWidth: number;
-}
+import MarqueeFooter from "../../component/missionList/marqueeFooter.tsx";
 
 const MissionList = () => {
-    const text = "캘리포니아 주의 안전은 SWAT 대원들에 의해 보호되고 있습니다. 안심하십시오";
-    const containerRef = useRef<HTMLDivElement>(null);
-    const textRefs = useRef<Array<TextRefs | null>>([]);
-
-    useEffect(() => {
-        if (!containerRef.current || !textRefs.current[0] || !textRefs.current[1]) return;
-
-        const items = textRefs.current.filter((item): item is TextRefs => item !== null);
-        const tl = horizontalLoop(items, {
-            repeat: -1,
-            speed: 1,
-            snap: false,
-            paddingRight: 50
-        });
-
-        return () => {
-            tl.kill();
-        };
-    }, []);
-
-    const horizontalLoop = (items: TextRefs[], config: HorizontalLoopConfig) => {
-        const tl = gsap.timeline({
-            repeat: config.repeat,
-            defaults: { ease: "none" }
-        });
-
-        const length = items.length;
-        const startX = items[0].offsetLeft;
-        const widths: number[] = [];
-        const xPercents: number[] = [];
-        const pixelsPerSecond = (config.speed || 1) * 100;
-
-        gsap.set(items, {
-            xPercent: (i: number, el: Element): number => {
-                const w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px") as string);
-                xPercents[i] = (parseFloat(gsap.getProperty(el, "x", "px") as string) / w * 100) + (gsap.getProperty(el, "xPercent") as number);
-                return xPercents[i];
-            }
-        });
-
-        gsap.set(items, { x: 0 });
-
-        const totalWidth = items[length - 1].offsetLeft +
-            xPercents[length - 1] / 100 * widths[length - 1] -
-            startX +
-            items[length - 1].offsetWidth * (gsap.getProperty(items[length - 1], "scaleX") as number) +
-            (config.paddingRight || 0);
-
-        items.forEach((item, i) => {
-            const curX = xPercents[i] / 100 * widths[i];
-            const distanceToStart = item.offsetLeft + curX - startX;
-            const distanceToLoop = distanceToStart + widths[i] * (gsap.getProperty(item, "scaleX") as number);
-
-            tl.to(item, {
-                xPercent: ((curX - distanceToLoop) / widths[i] * 100),
-                duration: distanceToLoop / pixelsPerSecond
-            }, 0)
-                .fromTo(item,
-                    { xPercent: ((curX - distanceToLoop + totalWidth) / widths[i] * 100) },
-                    {
-                        xPercent: xPercents[i],
-                        duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
-                        immediateRender: false
-                    },
-                    distanceToLoop / pixelsPerSecond
-                );
-        });
-
-        return tl;
-    };
-
     return (
         <div className={style.wrap}>
             <div className={style.container}>
@@ -92,14 +9,7 @@ const MissionList = () => {
                     <header className={style.accessorDataHeader}>
                         <span>Emily님 다시 오신것을 환영합니다</span>
                     </header>
-                    <footer className={style.accessorDataFooter}>
-                        <div className={style.warningTape}></div>
-                        <div ref={containerRef} className={style.textContainer}>
-                            <span style={{opacity: 0.6}} ref={el => textRefs.current[0] = el}>{text}</span>
-                            <span style={{opacity: 0.6}} ref={el => textRefs.current[1] = el}>{text}</span>
-                        </div>
-                        <div className={`${style.warningTape} ${style.bottom}`}></div>
-                    </footer>
+                    <MarqueeFooter text={'캘리포니아 주의 안전은 SWAT 대원들에 의해 보호되고 있습니다. 안심하십시오'} />
                 </aside>
             </div>
         </div>
