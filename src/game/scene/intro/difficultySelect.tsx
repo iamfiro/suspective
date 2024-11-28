5fd1576cea137ed6372de09931c4069d9143a5f9
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-28 12:25:17 +0900

    difficultySelect Scene 개발

--- src/game/scene/intro/difficultySelect.tsx
+++ src/game/scene/intro/difficultySelect.tsx
@@ -1,20 +1,22 @@
-import {Canvas} from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import style from '../../../styles/scene/difficultySelect.module.scss';
-import DifficultySelectRoom from "../../component/intro/difficultySelect/room.tsx";
 import CameraController from "../../component/cameraController.tsx";
+import Monitor from "../../component/intro/difficultySelect/monitor.tsx";
+import Desk from "../../component/intro/difficultySelect/desk.tsx";
+import PoliceStation from "../../component/intro/difficultySelect/policeStation.tsx";
 
-
-
-const difficultySelect = () => {
-	return (
-		<Canvas className={style.container} style={{height: '100vh', width: '100%'}}>
-			<ambientLight intensity={Math.PI / 2}/>
-			<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI}/>
-			<pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI}/>
-			<DifficultySelectRoom />
-			<CameraController />
-		</Canvas>
-	)
+const DifficultySelect = () => {
+    return (
+        <Canvas className={style.container} style={{height: '100vh', width: '100%'}}>
+            <ambientLight intensity={Math.PI / 2}/>
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI}/>
+            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI}/>
+            <Monitor />
+            <CameraController />
+            <PoliceStation />
+            <Desk />
+        </Canvas>
+    )
 }
 
-export default difficultySelect;
+export default DifficultySelect;
