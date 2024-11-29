69396ffef232a490ccc8833d7719251d709888df
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-29 12:15:47 +0900

    main.tsx에 mapbox-gl css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/global.scss'
+import './styles/global.scss';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
