3cef2f32e71a4a9f7b63ee3385fd539924d804b1
iamfiro <appturbo102@gmail.com>
2024-11-30 18:57:11 +0900

    배경화면 변경 및 시계 디자인

--- src/game/scenes/macbook/loginScreen.tsx
+++ src/game/scenes/macbook/loginScreen.tsx
@@ -2,10 +2,16 @@ import style from '../../../styles/scene/macbook/loginScreen.module.scss';
 
 export const MacLoginScreen = () => {
     return (
-        <main className={style.container}>
+        <div className={style.container}>
             <header className={style.controlCenter}>
                 <img src="/images/macbook/control/battery.svg" className={style.battery} alt="배터리 충전중"/>
-            </header>sa
-        </main>
+            </header>
+            <main className={style.content}>
+                <section className={style.time}>
+                    <h2>11월 30일 토요일</h2>
+                    <h1>6:46</h1>
+                </section>
+            </main>
+        </div>
     )
 }
