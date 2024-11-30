fd5de46b21627d6ac3b90679f23e98e1e5390b48
iamfiro <appturbo102@gmail.com>
2024-11-30 17:32:39 +0900

    기획 갈아엎어서 기존 데이터 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import IntroScene from "./game/scene/intro/introScene";
 import {useEffect, useState} from "react";
 import {createSceneManager, SceneRenderer} from "./engine/scene/sceneManager.tsx";
-import FictionDisclaimer from "./game/scene/intro/fictionDisclaimer.tsx";
-import WelcomeToStation from "./game/scene/intro/welcomeToStation.tsx";
-import IntranetBoot from "./game/scene/intranet/boot.tsx";
-import IntranetBootOS from "./game/scene/intranet/bootOS.tsx";
-import BootProgram from "./game/scene/intranet/bootProgram.tsx";
-import IntranetLogin from "./game/scene/intranet/login.tsx";
-import DifficultySelect from "./game/scene/intranet/DifficultySelect.tsx";
-import MissionList from "./game/scene/intranet/missionList.tsx";
 
 function App() {
     const [isInitialized, setIsInitialized] = useState(false);
@@ -19,39 +11,6 @@ function App() {
                 path: '/intro',
                 component: () => <IntroScene />
             },
-            {
-                path: '/disclaimer',
-                component: () => <FictionDisclaimer />
-            },
-            {
-                path: '/welcomeToStation',
-                component: () => <WelcomeToStation />
-            },
-            // intraNet
-            {
-                path: '/intranetBoot',
-                component: () => <IntranetBoot />
-            },
-            {
-                path: '/intranetBootOS',
-                component: () => <IntranetBootOS />
-            },
-            {
-                path: '/intranetBootProgram',
-                component: () => <BootProgram />
-            },
-            {
-                path: '/intranetLogin',
-                component: () => <IntranetLogin />
-            },
-            {
-                path: '/difficultySelect',
-                component: () => <DifficultySelect />
-            },
-            {
-                path: '/missionList',
-                component: () => <MissionList />
-            }
         ]);
         setIsInitialized(true);
     }, []);
@@ -62,7 +21,7 @@ function App() {
 
     return (
         <>
-            <SceneRenderer initialScene={'/missionList'} />
+            <SceneRenderer initialScene={'/intro'} />
         </>
     )
 }
