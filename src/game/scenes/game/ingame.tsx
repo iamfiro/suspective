29768357b67c236d08b819e7a6e8d268bd0b2a76
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-12-05 15:13:45 +0900

    범죄자 애니메이션 보완

--- src/game/scenes/game/ingame.tsx
+++ src/game/scenes/game/ingame.tsx
@@ -9,7 +9,7 @@ export const SceneInGame = () => {
 					<span>한유찬, 57세</span>
 				</article>
 				<article className={style.suspectItem}>
-					<img className={style.suspectImage} src={SuspectImage} alt={'범죄자 새끼'}/>
+					<img className={style.suspectImage} src={'https://img.wowtv.co.kr/wowtv_news/dnrs/20220930/2022093010334703116d3244b4fed182172185139.jpg'} alt={'범죄자 새끼'}/>
 					<span>한유찬, 57세</span>
 				</article>
 				<article className={style.suspectItem}>
